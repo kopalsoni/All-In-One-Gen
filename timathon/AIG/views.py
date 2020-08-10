a8d2751ac0269c00f49757225c29c2def78a4c02
tohtohro <E0031197@u.nus.edu>
2020-08-11 00:08:09 +0800

    Set up view templates

--- timathon/AIG/views.py
+++ timathon/AIG/views.py
@@ -13,3 +13,6 @@ def home(request):
     query = random_quote()
     print(query)
     return render(request, "home.html", {"data": query})
+
+def about(request): 
+    return render(request,"about.html")
