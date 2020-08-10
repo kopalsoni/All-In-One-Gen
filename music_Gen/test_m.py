905785d784e0ac8acf5749c0c9e270eac76a0826
sarzz2 <sarthaksingh2001@gmail.com>
2020-08-10 22:21:34 +0530

    refactoring for better organisation

--- music_Gen/test_m.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system("melody_rnn_generate \
---config=attention_rnn \
- --bundle_file=attention_rnn.mag \
---output_dir=/Users/singhyogendra/randomgen/AllInOne-Generator/tempmusic \
-num_output=20 \
-num_steps=200 \
-primer_melody='[120]'")
