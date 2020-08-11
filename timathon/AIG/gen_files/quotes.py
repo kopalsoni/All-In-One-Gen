eab3e817a6c308fabd95cbb93abca5413f300580
blakebiz <57645912+blakebiz@users.noreply.github.com>
2020-08-11 12:37:01 -0400

    Update quotes.py
    Added formatted_qoute function

--- timathon/AIG/gen_files/quotes.py
+++ timathon/AIG/gen_files/quotes.py
@@ -2,11 +2,21 @@ import requests
 import json
 
 
-def random_quote():
+def random_quote(formatted=False):
     url = "https://favqs.com/api/qotd"
     response = requests.request("GET", url)
-    x = json.loads(response.text)
-    return x["quote"]["body"]
+    x = json.loads(response.text)["quote"]
+    author, body = x["author"], x["body"]
+    return author, body if formatted else body
+
+
+def formatted_quote():
+    author, quote = random_quote(formatted=True)
+    return f'Author: {author}\nQuote: {quote}'
+
+
+if __name__ == '__main__':
+    print(formatted_quote())
 
 
 
